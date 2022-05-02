b61954d65f22028d49e72905489f8844d552f316
Kassem <systemry420@gmail.com>
2022-05-02 23:58:59 +0300

    define pages and routes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,31 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { 
   Navbar
 } from './components';
+import Home from './pages/Home';
+import Exchanges from './pages/Exchanges';
+import Cryptocurrencies from './pages/Cryptocurrencies';
+import CryptoDetails from './pages/CryptoDetails';
+import News from './pages/News';
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
+    <div className="app">
       <div className='navbar'>
         <Navbar />
       </div>
       <div className='main'>
-          <Routes>
-            
-          </Routes>
+          <Layout>
+            <div>
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/exchanges' element={<Exchanges />} />
+                <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+                <Route exact path='/news' element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
       </div>
       <div className='footer'>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,15 +7,31 @@ const Navbar = () => {
   return (
     <div className='nav-container'>
         <div className='logo-container'>
-            <Avatar />
+            <Avatar size={'large'} />
             <Typography.Title level={2} className='logo'>
                 <Link to='/'>Cryptoverse</Link>
             </Typography.Title>
             {/* <Button className='menu-control-container'>
 
             </Button> */}
-        </div>
 
+        </div>
+        <Menu theme='dark'>
+            <Menu.ItemGroup>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to='/news'>News</Link>
+                </Menu.Item>
+            </Menu.ItemGroup>
+        </Menu>
     </div>
   )
 }
