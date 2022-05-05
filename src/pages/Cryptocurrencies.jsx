7f6018626453ec526f2da6029874a25aa69e1c37
Kassem <systemry420@gmail.com>
2022-05-05 22:30:17 +0300

    display top 50 coins

--- src/pages/Cryptocurrencies.jsx
+++ src/pages/Cryptocurrencies.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Card, Input, Row, Col } from 'antd'
+import { Link } from 'react-router-dom'
+import { millify } from 'millify'
 
 const Cryptocurrencies = ({ coins }) => {
-  console.log(coins);
   
   return (
-    <div>Cryptocurrencies</div>
+    <>
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {coins.map(coin => (
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={coin.uuid}>
+            <Link to={`/crypto/${coin.uuid}`}>
+              <Card 
+                title={`${coin.rank}. ${coin.name} (${coin.symbol})`}
+                hoverable
+                extra={<img src={coin.iconUrl} className='crypto-image' alt={coin.name} />}
+              >
+                <p>Price: {Number(coin.price).toFixed(2)}</p>
+                <p>Market cap: {millify(coin.marketCap)}</p>
+                <p>24h Change: {coin.change}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
