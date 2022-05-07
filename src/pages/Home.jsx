21e909b44d751c9da7e1c70ecbc7c2039aa9392a
Kassem <systemry420@gmail.com>
2022-05-07 20:04:32 +0300

    display news cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,11 +45,10 @@ const Home = () => {
         <div className='home-heading-component'>
           <Title level={2}>Top 50 Coins</Title>
         </div>
-        <Cryptocurrencies coins={data.data.coins} />
+        {/* <Cryptocurrencies coins={data.data.coins} /> */}
 
         <div className='home-heading-component'>
           <Title level={2}>Latest News</Title>
-          <Title level={3}><Link to='/news'>Show more</Link></Title>
         </div>
         <News /> 
         
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -8,13 +8,31 @@ const { Option } = Select
 const News = () => {
   const {data, isFetching } = useGetNewsQuery({ category: 'Cryptocurrency', count: 8})
 
-  console.log(data);
-
-  return (
-    <div>
-
-    </div>
-  )
+  if (data) {
+    console.log(data);
+    return (
+      <Row gutter={[24, 24]}>
+        {data?.value?.map((news, i) => (
+          <Col xs={24} sm={12} lg={8} key={i}>
+            <Card hoverable className='news-card'>
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className='news-image-container'>
+                  <Title className='news-title' level={4}>{news.name}</Title>
+                  <img src={news?.image?.thumbnail?.contentUrl} alt={news.name} />
+                </div>
+                <p>
+                  {news.description > 100
+                    ? `${news.description.substring(0, 100)} ...`
+                    : news.description
+                  }
+                </p>
+              </a>
+            </Card>
+          </Col> 
+        ))}
+      </Row>
+    )
+  }
 }
 
 export default News
